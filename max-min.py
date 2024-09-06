5fadefd0f34bc90b3709e105cb144e9ba49e2d00
emmanescence <64655124+emmanescence@users.noreply.github.com>
2024-09-05 22:23:51 -0300

    Update max-min.py

--- max-min.py
+++ max-min.py
@@ -8,38 +8,6 @@ tickers_adrs = [
     'TGS', 'YPF', 'MELI', 'DESP', 'GLOB', 'BIOX', 'ARCO', 'TX', 'TS', 'AGRO'
 ]
 
-tickers_cedears = [
-    'SHPW', 'AABA', 'AOCA', 'BA.C', 'CAJ', 'LFC', 'SNP', 'CBRD', 'CS', 'AKO.B',
-    'F', 'HNPIY', 'MBT', 'NTCO', 'NLM', 'UN', 'PCRF', 'PTR', 'OGZD', 'LKOD',
-    'ATAD', 'SMSN', 'SI', 'TTM', 'RCTB4', 'TWTR', 'AUY', 'AMZN', 'NVDA', 'GOOGL',
-    'KO', 'SPY', 'VIST', 'COIN', 'AAPL', 'JMIA', 'SHOP', 'BBD', 'TSLA', 'INTC',
-    'MELI', 'MSTR', 'NKE', 'WMT', 'GOLD', 'MSFT', 'AMD', 'BABA', 'VALE', 'QQQ',
-    'GLOB', 'LAC', 'RIOT', 'DISN', 'DIA', 'AVGO', 'PFE', 'MCD', 'META', 'AAL',
-    'SATL', 'SBUX', 'JNJ', 'IWM', 'BITF', 'BRKB', 'PYPL', 'PEP', 'STLA', 'JD',
-    'ZM', 'ARKK', 'UPST', 'WBA', 'DESP', 'EEM', 'HMY', 'XLE', 'MMM', 'EWZ',
-    'V', 'PAAS', 'BIDU', 'CVX', 'QCOM', 'SQ', 'XOM', 'T', 'PLTR', 'PG', 'ROKU',
-    'NFLX', 'JPM', 'ABNB', 'TEN', 'MU', 'IBM', 'AAP', 'SNOW', 'XP', 'TXR',
-    'ADBE', 'BB', 'TM', 'SE', 'HL', 'XLF', 'GM', 'OXY', 'FSLR', 'LYG', 'VZ',
-    'BIOX', 'SNAP', 'SPOT', 'ETSY', 'BA', 'BP', 'CSCO', 'PANW', 'ABEV', 'ACN',
-    'ADGO', 'MUX', 'AXP', 'DE', 'CVS', 'WFC', 'HD', 'C', 'MA', 'TRIP', 'GE',
-    'CAT', 'WBO', 'MO', 'TXN', 'RACE', 'MRVL', 'TEFO', 'TWLO', 'SONY', 'MRNA',
-    'NEM', 'DOCU', 'LRCX', 'ABBV', 'TV', 'UBER', 'ITUB', 'ARCO', 'MDLZ', 'BRFS',
-    'COST', 'PBR', 'LMT', 'SPGI', 'X', 'BAK', 'PBI', 'STNE', 'GLW', 'CAR',
-    'CX', 'HAPV3', 'UL', 'MRK', 'GPRK', 'UAL', 'DAL', 'AMAT', 'AEM', 'BKNG',
-    'ERJ', 'SLB', 'AMGN', 'PAGS', 'BBV', 'ERIC', 'BBAS3', 'HSBC', 'PKS', 'GILD',
-    'BSBR', 'SPCE', 'NOKA', 'SHEL', 'ORCL', 'RBLX', 'DEO', 'CL', 'SDA', 'GS',
-    'PCAR', 'AZN', 'HAL', 'LVS', 'MGLU3', 'INFY', 'UNP', 'EA', 'BHP', 'ASR',
-    'GGB', 'RENT3', 'VIV', 'CCL', 'KGC', 'XROX', 'BIIB', 'AMX', 'PHG', 'NTES',
-    'ADI', 'GSK', 'USB', 'RTX', 'HWM', 'AIG', 'HON', 'NUE', 'HOG', 'SID',
-    'SAN', 'SYY', 'MOS', 'FDX', 'UGP', 'SAP', 'BCS', 'KOFM', 'NG', 'YY',
-    'MFG', 'PAC', 'NMR', 'EBAY', 'BK', 'MUFG', 'HDB', 'LND', 'AEG', 'PM',
-    'ORAN', 'GFI', 'MSI', 'KMB', 'IP', 'FMX', 'ROST', 'TTE', 'PRIO3', 'LREN3',
-    'EFX', 'PINS', 'DD', 'CAH', 'CAAP', 'SUZ', 'SCCO', 'AVY', 'TIMB', 'E',
-    'HMC', 'VRSN', 'SNA', 'ING', 'IBN', 'HPQ', 'ADP', 'GRMN', 'KEP', 'EBR',
-    'MMC', 'TRVV', 'TCOM', 'ELP', 'IFF', 'URBN', 'SBS', 'KB', 'YELP', 'NEC1',
-    'LLY', 'FCX'
-]
-
 tickers_panel_lider = [
     'ALUA.BA', 'BBAR.BA', 'BMA.BA', 'BYMA.BA', 'CEPU.BA', 'COME.BA', 'CRES.BA', 'CVH.BA', 'EDN.BA',
     'GGAL.BA', 'HARG.BA', 'LOMA.BA', 'MIRG.BA', 'PAMP.BA', 'SUPV.BA', 'TECO2.BA', 'TGNO4.BA', 'TGSU2.BA',
@@ -104,20 +72,16 @@ def display_tickers(tickers):
 
 # Interfaz principal
 st.title('Información de Tickers')
-tab1, tab2, tab3, tab4 = st.tabs(["ADR", "CEDEAR", "Panel Líder", "Panel General"])
+tab1, tab2, tab3 = st.tabs(["ADR", "Panel Líder", "Panel General"])
 
 with tab1:
     st.write("### ADRs")
     display_tickers(tickers_adrs)
 
 with tab2:
-    st.write("### CEDEARs")
-    display_tickers(tickers_cedears)
-
-with tab3:
     st.write("### Panel Líder")
     display_tickers(tickers_panel_lider)
 
-with tab4:
+with tab3:
     st.write("### Panel General")
     display_tickers(tickers_panel_general)
