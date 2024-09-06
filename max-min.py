bc5a2c7fda6cee7ecdffda3713723cd3964aeff8
emmanescence <64655124+emmanescence@users.noreply.github.com>
2024-09-05 23:26:34 -0300

    Update max-min.py

--- max-min.py
+++ max-min.py
@@ -1,8 +1,6 @@
 import streamlit as st
 import yfinance as yf
 import pandas as pd
-import matplotlib.pyplot as plt
-import io
 
 # Define los grupos de tickers
 tickers_adrs = [
@@ -61,20 +59,6 @@ def get_ticker_info(ticker):
         st.error(f"Error fetching data for {ticker}: {e}")
         return None, None, None, None, None
 
-# Función para guardar un DataFrame como imagen
-def save_dataframe_as_image(df, filename):
-    fig, ax = plt.subplots(figsize=(12, 8))
-    ax.axis('off')
-    tbl = ax.table(cellText=df.values, colLabels=df.columns, cellLoc='center', loc='center', colColours=['#f5f5f5']*len(df.columns))
-    tbl.auto_set_font_size(False)
-    tbl.set_fontsize(10)
-    tbl.scale(1.2, 1.2)
-
-    buf = io.BytesIO()
-    plt.savefig(buf, format='png', bbox_inches='tight', pad_inches=0.1)
-    buf.seek(0)
-    return buf
-
 # Función para mostrar los tickers
 def display_tickers(tickers):
     results = []
@@ -103,30 +87,11 @@ def display_tickers(tickers):
         df = df.hide(axis='index')  # Eliminar el índice
         return df
 
-    # Mostrar y guardar como imagen
     st.write("### Top 10 con Mayor Drawdown")
-    top_10_farthest_formatted = format_table(top_10_farthest)
-    st.write(top_10_farthest_formatted.to_html(), unsafe_allow_html=True)
-
-    image_farthest = save_dataframe_as_image(top_10_farthest, 'top_10_farthest.png')
-    st.download_button(
-        label="Descargar imagen Top 10 con Mayor Drawdown",
-        data=image_farthest,
-        file_name='top_10_farthest.png',
-        mime='image/png'
-    )
+    st.write(format_table(top_10_farthest).to_html(), unsafe_allow_html=True)
 
     st.write("### Top 10 con Menor Drawdown")
-    top_10_closest_formatted = format_table(top_10_closest)
-    st.write(top_10_closest_formatted.to_html(), unsafe_allow_html=True)
-
-    image_closest = save_dataframe_as_image(top_10_closest, 'top_10_closest.png')
-    st.download_button(
-        label="Descargar imagen Top 10 con Menor Drawdown",
-        data=image_closest,
-        file_name='top_10_closest.png',
-        mime='image/png'
-    )
+    st.write(format_table(top_10_closest).to_html(), unsafe_allow_html=True)
 
 # Interfaz principal
 st.title('Precios máximos históricos y actuales - https://x.com/iterAR_eco')
@@ -143,4 +108,3 @@ with tab2:
 with tab3:
     st.write("### Panel General")
     display_tickers(tickers_panel_general)
-
